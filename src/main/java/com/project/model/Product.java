c2c1468a9220c967f96f6fdd532e2b0eee476a9f
malakouti.aero@gmail.com <malakouti.aero@gmail.com>
2023-06-27 00:35:30 +0330

    git khol shode

---
+++ src/main/java/com/project/model/Product.java
@@ -0,0 +1,25 @@
+package com.project.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id",nullable = false)
+    private Category category;
+
+}
